7bc86b454b8a2401bacc7c43ead6dbf13af71063
latif83 <latifm8360@gmail.com>
2022-10-27 12:17:01 +0000

    .

---
+++ src/app/data.js
@@ -0,0 +1,14 @@
+export const x = [
+    {
+    "name" : "Paracetamol",
+    "price" : 200,
+    "dosage" : "500 mg",
+    "image" : ""
+},
+{
+    "name" : "Paracetamol",
+    "price" : 200,
+    "dosage" : "500 mg",
+    "image" : ""
+}
+]
