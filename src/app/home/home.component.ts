6793820fd7d4ee991a43d5ea72c6ad157c899468
latif83 <latifm8360@gmail.com>
2022-10-27 12:51:19 +0000

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { x } from './../data';
+import { data } from '../pharm';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +9,18 @@ import { x } from './../data';
 })
 export class HomeComponent implements OnInit {
 
+  medicines:any = []
+
   constructor() { }
 
   ngOnInit(): void {
     let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+data.forEach((each:any)=>{
+  this.medicines.push(each)
+})
+
   }
 
 }
---
+++ src/app/pharm.ts
@@ -0,0 +1,30 @@
+export const data = [
+    {
+    "name" : "Bell Paracetamol",
+    "price" : 5,
+    "dosage" : "500 mg",
+    "image" : "",
+    "category" : "Pain killer"
+},
+{
+    "name" : "Emzor Paracetamol",
+    "price" : 10,
+    "dosage" : "500 mg",
+    "image" : "",
+    "category" : "pain killer"
+},
+{
+    "name" : "Emzor Paracetamol",
+    "price" : 20,
+    "dosage" : "500 mg",
+    "image" : "",
+    "category" : "pain killer"
+},
+{
+    "name" : "Wormplex 400",
+    "price" : 20,
+    "dosage" : "500 mg",
+    "image" : "",
+    "category" : "Dewormer"
+}
+]
