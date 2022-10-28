50f9feef891d2bca953d8618f96c88b146d68c7b
latif83 <latifm8360@gmail.com>
2022-10-28 08:03:31 +0000

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // @ViewChild('desktop') desRef: ElementRef | undefined;
+  // des: HTMLElement;
   title = 'pharmacy-k';
+
+  constructor(){
+    let router = document.getElementById('app') as HTMLElement
+    let desktop = document.getElementById('desktop') as HTMLElement
+    
+    // alert(window.innerWidth)
+
+    // if(window.innerWidth > 400){
+    //   desktop.classList.remove('d-none')
+    //   router.classList.add('d-none')
+    // }
+    // else{
+    //   desktop.classList.add('d-none')
+    //   router.classList.remove('d-none')
+    // }
+  }
 }
